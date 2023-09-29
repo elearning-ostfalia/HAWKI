648269c7f91718a6edec9fc4961e98d81cfd30fb
Karin Borm <k.borm@ostfalia.de>
2023-09-29 11:07:04 +0200

    do not comment out most menu items

--- interface.php
+++ interface.php
@@ -28,7 +28,7 @@ if (!isset($_SESSION['username'])) {
 		<svg viewBox="0 0 24 24"><path d="M20 2H4C2.9 2 2 2.9 2 4V22L6 18H20C21.1 18 22 17.1 22 16V4C22 2.9 21.1 2 20 2M20 16H5.2L4 17.2V4H20V16Z" /></svg>
 		Chat
 	  </div>
-<!--
+
 	  <details>
 		  <summary>
 			  <h3>Virtuelles Büro ⓘ</h3>
@@ -92,7 +92,7 @@ if (!isset($_SESSION['username'])) {
 			<div class="submenu-item" onclick="load(this, 'interview.htm')">Interviewfragen</div>
 			<div class="submenu-item" onclick="load(this, 'prototyping.htm')">Prototyping</div>
 		</div>
-	-->
+
 
 	</div>
 	<div class="info">
