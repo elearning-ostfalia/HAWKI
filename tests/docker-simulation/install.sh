0d86c4dfb4668bbac0cca00ade84cd40749b429b
Karin Borm <k.borm@ostfalia.de>
2025-03-10 14:37:21 +0100

    next step

--- tests/docker-simulation/install.sh
+++ tests/docker-simulation/install.sh
@@ -52,12 +52,36 @@ php artisan migrate --force
 echo 'database seed'
 php artisan db:seed --force
 
+
+
+
+#systemctl daemon-reload
+
+#systemctl enable reverb.service
+#systemctl enable laravel-worker.service
+
+#systemctl start reverb.service
+#systemctl start laravel-worker.service
+
+#systemctl status reverb.service
+#systemctl status laravel-worker.service
+
+echo 'start Websocket server'
+php artisan reverb:start &
+
+# start "services"
+
+echo 'start Laravel Worker Service 1'
+php artisan queue:work &
+echo 'start Laravel Worker Service 2'
+php artisan queue:work --queue=mails &
+echo 'start Laravel Worker Service 3'
+php artisan queue:work --queue=message_broadcast &
+
+#su www-data php artisan queue:work --queue=default,mails,message_broadcast
+
 echo 'installation finished'
 touch /tmp/OLAF_INSTALLED.txt
-
-
-echo 'start reverb'
-php artisan reverb:start &
 
 # start apache
 echo 'start apache'
